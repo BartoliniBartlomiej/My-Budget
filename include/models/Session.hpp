8d18aee6102329be02832321805cf7df3565603b
bartek <bartek.k2906@gmail.com>
2026-06-23 17:16:00 +0200

    feat: Session added

--- include/models/Session.hpp
+++ include/models/Session.hpp
@@ -3,14 +3,18 @@
 #include <string>
 
 class Session {
+private:
     int         userId;
     std::string username;
     bool        loggedIn;
 
 public:
-    Session(int _userId, std::string _username) : userId(_userId), username(_username) {}
+    Session(int _userId, std::string _username) 
+        : userId(_userId), username(_username), loggedIn(true) {}
 
     int         getUserId()   const {   return userId;     }
     std::string getUsername() const {   return username;   }
+    bool        isLoggedIn()  const {   return loggedIn;   }
     
+    void logout() { loggedIn = false; }
 };
--- src/models/Session.cpp
+++ src/models/Session.cpp
@@ -1,2 +1,1 @@
 #include "../../include/models/Session.hpp"
-
