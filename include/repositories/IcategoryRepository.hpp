3b5438abc32d44817b9e53d85c258cc012b73b1e
bartek <bartek.k2906@gmail.com>
2026-06-24 14:11:55 +0200

    fix: filename update

--- include/repositories/IcategoryRepository.hpp
+++ include/repositories/IcategoryRepository.hpp
@@ -9,6 +9,8 @@ public:
     virtual ~ICategoryRepository() = default;
 
     virtual void                    add(const Category& category)       = 0;
+
     virtual std::optional<Category> getById(int id)               const = 0;
+    
     virtual std::vector<Category>   getByUserId(int userId)       const = 0;
 };
