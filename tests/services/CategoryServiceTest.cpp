d41657a4ab370c15d08e380bec87ad87246cbd79
bartek <bartek.k2906@gmail.com>
2026-06-24 14:00:41 +0200

    test: add unit tests for CategoryService

---
+++ tests/services/CategoryServiceTest.cpp
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+#include "services/CategoryService.hpp"
+#include "repositories/InMemoryCategoryRepository.hpp"
+
+TEST(CategoryServiceTest, InitializesDefaultCategories)
+{
+    InMemoryCategoryRepository repo;
+    CategoryService categoryService(repo);
+
+    categoryService.initializeDefaultCategories();
+
+    Session session(42, "smith");
+
+    auto categories = categoryService.getAvailableCategories(session);
+
+    EXPECT_EQ(categories.size(), 5);
+}
+
+TEST(CategoryServiceTest, CreatesPrivateCategoryForUser)
+{
+    InMemoryCategoryRepository repo;
+    CategoryService categoryService(repo);
+
+    categoryService.initializeDefaultCategories();
+
+    Session session(42, "smith");
+
+    EXPECT_TRUE(
+        categoryService.createCategory(
+            "My Hobby",
+            CategoryType::ENTERTAINMENT,
+            session));
+}
+
+TEST(CategoryServiceTest, UserSeesCreatedPrivateCategory)
+{
+    InMemoryCategoryRepository repo;
+    CategoryService categoryService(repo);
+
+    categoryService.initializeDefaultCategories();
+
+    Session session(42, "smith");
+
+    categoryService.createCategory(
+        "My Hobby",
+        CategoryType::ENTERTAINMENT,
+        session);
+
+    auto categories = categoryService.getAvailableCategories(session);
+
+    EXPECT_EQ(categories.size(), 6);
+}
+
+TEST(CategoryServiceTest, PrivateCategoryIsNotVisibleToOtherUsers)
+{
+    InMemoryCategoryRepository repo;
+    CategoryService categoryService(repo);
+
+    categoryService.initializeDefaultCategories();
+
+    Session ownerSession(42, "smith");
+    Session anotherSession(99, "tornvalds");
+
+    categoryService.createCategory(
+        "My Hobby",
+        CategoryType::ENTERTAINMENT,
+        ownerSession);
+
+    auto categories = categoryService.getAvailableCategories(anotherSession);
+
+    EXPECT_EQ(categories.size(), 5);
+}
