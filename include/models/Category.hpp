6ff26b140dfde8b4551f6567b0e5f5555bc2aeff
bartek <bartek.k2906@gmail.com>
2026-06-24 13:54:56 +0200

    feat: add Constructors

--- include/models/Category.hpp
+++ include/models/Category.hpp
@@ -12,6 +12,10 @@ private:
     int             userId;
 
 public:
+    Category() : id(0), name(""), type(CategoryType::OTHER), userId(0) {}
+    Category(std::string _name, CategoryType _type, int _userId) 
+        : id(0), name(_name), type(_type), userId(_userId) {}
+
     int          getId()     const {   return id;    }
     std::string  getName()   const {   return name;  }
     CategoryType getType()   const {   return type;  }
